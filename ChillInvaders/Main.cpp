21ec181d4a5753887e524927ee5718abf983ed6d
TheFloatingBrain <sub_email_here@example.com>
2025-05-04 21:56:49 -0400

    Add project files.

---
+++ ChillInvaders/Main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+
+int main(int argc, char** args)
+{
+	std::string name;
+	std::cout << "What is your name?: ";
+	std::getline(std::cin, name);
+	std::cout << "Hello " << name << " build succeeded.\n";
+	return 0;
+}
